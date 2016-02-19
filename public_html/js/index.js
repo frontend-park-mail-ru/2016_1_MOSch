16e0536a6e9ff52066a43789051e637390d70dfa
KOPTE3 <ostapencko.anatoliy@yandex.ru>
2016-02-19 19:09:56 +0300

    Плохая архитектура проекта

--- public_html/js/index.js
+++ public_html/js/index.js
@@ -21,11 +21,15 @@ function hideLoginScreen() {
 
 function showMainScreen() {
 	// Конструктор экрана "Гравный экран"
-	$page.html(mainTmpl()); // Рендерим шаблон
+	$page.html(mainTmpl({
+		data: [1,2,3,4,5]
+	}
+	)); // Рендерим шаблон
 	// Инициализируем обработчики событий
 	$page.find('.js-scoreboard')
 		.on('click', showScoreboardScreen);
 	$page.find('.js-start-game').on('click', showGameScreen);
+	document.title = "Заголовок Главного экрана";
 }
 
 function hideMainScreen() {
@@ -34,5 +38,6 @@ function hideMainScreen() {
 	$page.find('.js-scoreboard')
 		.off('click', showScoreboardScreen);
 	$page.find('.js-start-game').off('click', showGameScreen);
+	document.title = "Ушли с Главного экрана";
 }
 
