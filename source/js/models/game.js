046bec5ed8aed6841f92ad40bdc222e0e9857844
Anatoliy Ostapenko <ostapencko.anatoliy@yandex.ru>
2016-05-25 18:46:53 +0300

    fix 5

--- source/js/models/game.js
+++ source/js/models/game.js
@@ -34,29 +34,6 @@ define(function (require) {
 		}.bind(this));
 	};
 
-	var drawBgc = function () {
-		var viewportWidth = this._ctx.canvas.width;
-		var viewportHeight = this._ctx.canvas.height;
-		var offsetX = viewportWidth / 2;
-		var offsetY = viewportHeight * 1.1;
-		var outerRadius = Math.max(viewportHeight, viewportWidth) * 1.5;
-		var innerRadius = outerRadius * 0.02 + 5;
-
-		var first = 280 / outerRadius;
-		var second = 420 / outerRadius;
-		var gr = this._ctx.createRadialGradient(offsetX, offsetY, innerRadius, offsetX, offsetY, outerRadius);
-		gr.addColorStop(0, '#6666BB');
-		gr.addColorStop(first, '#1C1C29');
-		gr.addColorStop(second, '#0A0A1A');
-		gr.addColorStop(0.8, '#020209');
-		this._ctx.fillStyle = gr;
-
-		this._ctx.fillRect(0, 0, viewportWidth, viewportHeight);
-		this._ctx.fillStyle = 'black';
-		this._ctx.fillRect(25, 25, 100, 100);
-		this._ctx.clearRect(45, 45, 60, 60);
-		this._ctx.strokeRect(50, 50, 50, 50);
-	};
 
 	var destroy = function () {
 		this._engine.dispose();
